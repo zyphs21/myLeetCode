8c78a1ae09afc55b1504ed07b6a117c7f4270a2f
zyphs21 <hansenhs21@live.com>
2018-06-27 10:16:20 +0800

    add RemoveDuplicatesFromSortedArray

---
+++ MyLeetCode.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import PlaygroundSupport
+
+var testArrays: [Int] = [1,1,1,2,2,5,5,7]
+RemoveDuplicatesFromSortedArray.removeDuplicates(&testArrays)
+testArrays
